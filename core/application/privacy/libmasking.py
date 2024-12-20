fix bug application.masking

core/application/privacy/libmasking.py
@@ -161,7 +161,7 @@ class LibMasking:
 
     @staticmethod
     def scanningImage(path_image_or_bgr, path_out_json, **kwargs) -> typing.Tuple[VideoInfo, typing.Union[np.ndarray, None]]:
-        cache = XPortrait.toCache(path_image_or_bgr)
+        cache = XPortrait.packageAsCache(path_image_or_bgr)
         path_out_json = path_out_json or Resource.createRandomCacheFileName('.json')
         max_num = LibMasking.getFixedNumFromImage(path_image_or_bgr, kwargs.pop('max_num', -1))
         video_info = LibScaner.inference([cache], path_out_json=path_out_json, fixed_num=max_num)

core/application/privacy/libscaner.py
@@ -267,19 +267,8 @@ class LibScaner:
     """
     """
     @staticmethod
-    def toCache(source) -> typing.Union[XBody, XPortrait]:
-        return XPortrait.toCache(source)
-        # assert isinstance(source, (np.ndarray, str, XBody, XPortrait))
-        # if isinstance(source, XPortrait):
-        #     return source
-        # if isinstance(source, str):
-        #     if source.endswith('pkl'):
-        #         return XPortrait.load(source, verbose=False)
-        #     if source.endswith('png') or source.endswith('jpg'):
-        #         source = cv2.imread(source)
-        # if isinstance(source, np.ndarray):
-        #     return XPortrait.packageAsCache(source, asserting=False)
-        # raise NotImplementedError(source)
+    def packageAsCache(source) -> typing.Union[XBody, XPortrait]:
+        return XPortrait.packageAsCache(source)
 
     @staticmethod
     def toNdarray(source):
@@ -355,7 +344,7 @@ class LibScaner:
                 video_info = VideoInfo(fixed_num=fixed_num)
                 for n, source in enumerate(reader_iterator):
                     if n % sample_step == 0:
-                        cache = LibScaner.toCache(source)
+                        cache = LibScaner.packageAsCache(source)
                         LibScaner.updateCommon(n, cache, video_info)
                     bar.update(1)
                 video_info.updatePersonList([])  # end the update

core/base/cache/xportrait.py
@@ -38,19 +38,18 @@ class XPortrait(XCache):
 
     @staticmethod
     def packageAsCache(source, **kwargs):
-        assert isinstance(source, (np.ndarray, XPortrait)), type(source)
-        cache = XPortrait(bgr=source, **kwargs) if isinstance(source, np.ndarray) else source
-        return cache
-
-    @staticmethod
-    def toCache(source, **kwargs):
         assert isinstance(source, (str, np.ndarray, XPortrait)), type(source)
         if isinstance(source, str):
             if source.endswith('pkl'):
-                source = XPortrait.load(source, verbose=False)
-            if source.endswith('png') or source.endswith('jpg'):
-                source = cv2.imread(source)
-        return XPortrait.packageAsCache(source, asserting=False)
+                return XPortrait.load(source, verbose=False)
+            if source.endswith('png') or source.endswith('jpg') or source.endswith('bmp'):
+                return XPortrait(bgr=cv2.imread(source), **kwargs)
+        if isinstance(source, np.ndarray):
+            assert len(source.shape) == 2 or (len(source.shape) == 3 and source.shape[2] == 3), source.shape
+            return XPortrait(bgr=source, **kwargs)
+        if isinstance(source, XPortrait):
+            return source
+        raise NotImplementedError(source)
 
     """
     global config