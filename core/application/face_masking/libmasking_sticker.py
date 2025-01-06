fix bug application.masking -- bbox

core/application/face_masking/libmasking_sticker.py
@@ -156,7 +156,7 @@ class LibMasking_Sticker:
                 H, W, C = bgr.shape
                 box_src, box_fmt = sticker['box']
                 box_remap = BoundingBox.remapBBox(box_src, box_fmt, box)
-                lft, top, rig, bot = BoundingBox(box_remap).clip(0, 0, W, H).decouple()
+                lft, top, rig, bot = BoundingBox(np.array(box_remap, dtype=np.int32)).clip(0, 0, W, H).decouple()
                 h = bot - top
                 w = rig - lft
                 resized_sticker = cv2.resize(sticker_image, (w, h))