485a1f85f2f657f1d32042c5a264eb03dbe0cf37
xiaoye <hanxiongxiao@gmail.com>
2025-01-15 15:44:00 +0800

    update application.masking -- fix small bug in boundingbox.py (for version)

--- core/application/face_masking/__init__.py
+++ core/application/face_masking/__init__.py
@@ -1,11 +1,11 @@
 
 import logging
 
-__version__ = '0.1.2'
+__version__ = '0.1.3'
 logging.error('face_masking version: {}'.format(__version__))
 
 """
-version - 0.1.2
+version - 0.1.3
 note: fix bug in yolo tracking, reset tracker
 
 """
