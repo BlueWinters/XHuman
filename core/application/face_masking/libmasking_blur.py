update application.masking -- fix bug for masking near face

core/application/face_masking/libmasking_blur.py
@@ -9,7 +9,8 @@ import pickle
 import typing
 import tqdm
 # import numba
-from ...geometry import Rectangle
+from .boundingbox import Rectangle, BoundingBox
+from .morphology import getMaxRegion
 from ...base import XPortrait, XPortraitHelper
 from ...utils.context import XContextTimer
 from ...utils.video import XVideoReader, XVideoWriter
@@ -200,10 +201,32 @@ class LibMasking_Blur:
         return XPortraitHelper.getFaceRegion(cache, index=n, top_line=top_line, value=value)[n]
 
     @staticmethod
-    def getHeadMask(cache, value=255):
+    def getHeadMask(bgr, box, value=255):
+        lft, top, rig, bot = box
+        cache = XPortrait(bgr[top:bot, lft:rig, :])
+        if cache.number == 1:
+            parsing = cache.parsing
+            mask = np.where((0 < parsing) & (parsing < 15) & (parsing != 12), value, 0).astype(np.uint8)
+            return getMaxRegion(mask) * value
+        if cache.number > 1:
+            box_src = np.reshape(np.array(box, dtype=np.int32), (1, 4))
+            box_cur = np.reshape(np.array(cache.box, dtype=np.int32), (-1, 4))
+            iou = BoundingBox.computeIOU(boxes1=box_src, boxes2=box_cur)  # 1,N
+            part_copy = np.copy(cache.bgr)
+            selected_index = int(np.argmax(iou[0, :]))
+            for n in range(cache.number):
+                if n != selected_index:
+                    l, t, r, b = cache.box[n, :]
+                    part_copy[t:b, l:r, :] = 255
+            l, t, r, b = cache.box[selected_index, :]
+            part_copy[t:b, l:r, :] = cache.bgr[t:b, l:r, :]
+            parsing = XPortrait(part_copy).parsing
+            mask = np.where((0 < parsing) & (parsing < 15) & (parsing != 12), value, 0).astype(np.uint8)
+            return getMaxRegion(mask) * value
+        # note: detect face fail
         parsing = cache.parsing
         mask = np.where((0 < parsing) & (parsing < 15) & (parsing != 12), value, 0).astype(np.uint8)
-        return mask
+        return getMaxRegion(mask) * value
 
     @staticmethod
     def workOnSelected(source_bgr, blured_bgr, kernel=17, mask=None, **kwargs):
@@ -255,7 +278,7 @@ class LibMasking_Blur:
         elif focus_type == 'head':
             mask = np.zeros(shape=(h, w), dtype=np.uint8)
             lft, top, rig, bot = Rectangle(box).toSquare().expand(1., 1).clip(0, 0, w, h).asInt()
-            mask[top:bot, lft:rig] = LibMasking_Blur.getHeadMask(XPortrait(bgr[top:bot, lft:rig, :]))
+            mask[top:bot, lft:rig] = LibMasking_Blur.getHeadMask(bgr, (lft, top, rig, bot))
             mask[box[3]:, :] = 0
         else:
             mask = LibMasking_Blur.getMaskFromBox(h, w, box, ratio=0.)
@@ -273,7 +296,7 @@ class LibMasking_Blur:
         elif focus_type == 'head':
             mask = np.zeros(shape=(h, w), dtype=np.uint8)
             lft, top, rig, bot = Rectangle(box).toSquare().expand(1., 1.).clip(0, 0, w, h).asInt()
-            mask[top:bot, lft:rig] = LibMasking_Blur.getHeadMask(XPortrait(bgr[top:bot, lft:rig, :]))
+            mask[top:bot, lft:rig] = LibMasking_Blur.getHeadMask(bgr, (lft, top, rig, bot))
             mask[box[3]:, :] = 0
         else:
             mask = LibMasking_Blur.getMaskFromBox(h, w, box, ratio=0.)
@@ -290,7 +313,7 @@ class LibMasking_Blur:
         elif focus_type == 'head':
             mask = np.zeros(shape=(h, w), dtype=np.uint8)
             lft, top, rig, bot = Rectangle(box).toSquare().expand(1., 1.).clip(0, 0, w, h).asInt()
-            mask[top:bot, lft:rig] = LibMasking_Blur.getHeadMask(XPortrait(bgr[top:bot, lft:rig, :]))
+            mask[top:bot, lft:rig] = LibMasking_Blur.getHeadMask(bgr, (lft, top, rig, bot))
             mask[box[3]:, :] = 0
         else:
             mask = LibMasking_Blur.getMaskFromBox(h, w, box, ratio=0.)
@@ -314,7 +337,7 @@ class LibMasking_Blur:
         elif focus_type == 'head':
             mask = np.zeros(shape=(h, w), dtype=np.uint8)
             lft, top, rig, bot = Rectangle(box).toSquare().expand(1., 1.).clip(0, 0, w, h).asInt()
-            mask[top:bot, lft:rig] = LibMasking_Blur.getHeadMask(XPortrait(bgr[top:bot, lft:rig, :]))
+            mask[top:bot, lft:rig] = LibMasking_Blur.getHeadMask(bgr, (lft, top, rig, bot))
             mask[box[3]:, :] = 0
         else:
             mask = LibMasking_Blur.getMaskFromBox(h, w, box, ratio=0.)

core/application/face_masking/libmasking_mosaic.py
@@ -5,7 +5,8 @@ import cv2
 import numpy as np
 import tqdm
 from skimage import segmentation
-from ...geometry import Rectangle
+from .boundingbox import Rectangle, BoundingBox
+from .morphology import getMaxRegion
 from ...base.cache import XPortrait, XPortraitHelper
 from ...thirdparty.cache import XBody
 from ...utils.context import XContextTimer
@@ -104,7 +105,7 @@ class LibMasking_Mosaic:
             if focus_type == 'face':
                 mask[top:bot, lft:rig] = LibMasking_Mosaic.getFaceMaskByPoints(XPortrait(bgr[top:bot, lft:rig, :]))
             if focus_type == 'head':
-                mask[top:bot, lft:rig] = LibMasking_Mosaic.getHeadMask(XPortrait(bgr[top:bot, lft:rig, :]))
+                mask[top:bot, lft:rig] = LibMasking_Mosaic.getHeadMask(bgr, (lft, top, rig, bot))
                 mask[box[3]:, :] = 0
             return LibMasking_Mosaic.workOnSelected(cache.bgr, mosaic_bgr, mask=mask)
         else:
@@ -172,12 +173,31 @@ class LibMasking_Mosaic:
         return XPortraitHelper.getFaceRegion(cache, index=n, top_line=top_line, value=value)[n]
 
     @staticmethod
-    def getHeadMask(cache, value=255):
-        # cache = XBody(bgr, backend='ultralytics.yolo11n-pose')
-        # parsing = cache.portrait_parsing
+    def getHeadMask(bgr, box, value=255):
+        lft, top, rig, bot = box
+        cache = XPortrait(bgr[top:bot, lft:rig, :])
+        if cache.number == 1:
+            parsing = cache.parsing
+            mask = np.where((0 < parsing) & (parsing < 15) & (parsing != 12), value, 0).astype(np.uint8)
+            return getMaxRegion(mask) * value
+        if cache.number > 1:
+            box_src = np.reshape(np.array(box, dtype=np.int32), (1, 4))
+            box_cur = np.reshape(np.array(cache.box, dtype=np.int32), (-1, 4))
+            iou = BoundingBox.computeIOU(boxes1=box_src, boxes2=box_cur)  # 1,N
+            part_copy = np.copy(cache.bgr)
+            selected_index = int(np.argmax(iou[0, :]))
+            for n in range(cache.number):
+                if n != selected_index:
+                    l, t, r, b = cache.box[n, :]
+                    part_copy[t:b, l:r, :] = 255
+            part_copy[top:bot, lft:rig, :] = bgr[top:bot, lft:rig, :]
+            parsing = XPortrait(part_copy).parsing
+            mask = np.where((0 < parsing) & (parsing < 15) & (parsing != 12), value, 0).astype(np.uint8)
+            return getMaxRegion(mask) * value
+        # note: detect face fail
         parsing = cache.parsing
         mask = np.where((0 < parsing) & (parsing < 15) & (parsing != 12), value, 0).astype(np.uint8)
-        return mask
+        return getMaxRegion(mask) * value
 
     @staticmethod
     def getMaskFromBox(h, w, box, ratio, value=255):
@@ -210,7 +230,7 @@ class LibMasking_Mosaic:
             if focus_type == 'face':
                 mask[top:bot, lft:rig] = LibMasking_Mosaic.getFaceMaskByPoints(XPortrait(part))
             if focus_type == 'head':
-                mask[top:bot, lft:rig] = LibMasking_Mosaic.getHeadMask(XPortrait(part))
+                mask[top:bot, lft:rig] = LibMasking_Mosaic.getHeadMask(bgr, (lft, top, rig, bot))
                 mask[box[3]:, :] = 0
             mesh_size = int(size / 256 * 7 / 4)
             mesh_size = int(np.clip(mesh_size, 3, 13))

core/application/face_masking/libmasking_sticker.py
@@ -58,6 +58,15 @@ class LibMasking_Sticker:
         return canvas
 
     @staticmethod
+    def getIndexByBox(cache, box):
+        assert isinstance(cache, XPortrait), type(cache)
+        box_src = np.reshape(np.array(box, dtype=np.int32), (1, 4))
+        box_cur = np.reshape(np.array(cache.box, dtype=np.int32), (-1, 4))
+        iou = BoundingBox.computeIOU(boxes1=box_src, boxes2=box_cur)  # 1,N
+        print(iou.shape)
+        return int(np.argmax(iou[0, :]))
+
+    @staticmethod
     def toCache(source):
         assert isinstance(source, (np.ndarray, str, XPortrait))
         if isinstance(source, str):
@@ -114,21 +123,21 @@ class LibMasking_Sticker:
                 top = int(max(0, top - hh * ratio))
                 rig = int(min(w, rig + ww * ratio))
                 bot = int(min(h, bot + hh * ratio))
-                dst_h, dst_w = bot - top, rig - lft
                 part = bgr[top:bot, lft:rig, :]
                 cache = XPortrait(part)
-                points_sticker = sticker['eyes_center']
-                points_source = cache.points[0, :2]
+                points_sticker = np.array(sticker['eyes_center'], dtype=np.float32)
+                n = LibMasking_Sticker.getIndexByBox(cache, box)  # default is 0
+                points_source = cache.points[n, :2]
+                points_source[:, 0] += lft
+                points_source[:, 1] += top
                 matrix = cv2.estimateAffinePartial2D(points_sticker, points_source, method=cv2.LMEDS)[0]
-                param = dict(dsize=(dst_w, dst_h), flags=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT, borderValue=(255, 255, 255))
+                param = dict(dsize=(w, h), flags=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT, borderValue=(255, 255, 255))
                 sticker_warped = cv2.warpAffine(sticker_image, matrix, **param)
                 sticker_warped_bgr, sticker_warped_alpha = sticker_warped[:, :, :3], sticker_warped[:, :, 3:4]
                 mask = sticker_warped_alpha.astype(np.float32) / 255.
-                fusion = part * (1 - mask) + sticker_warped_bgr * mask
+                fusion = bgr * (1 - mask) + sticker_warped_bgr * mask
                 fusion_bgr = np.round(fusion).astype(np.uint8)
-                bgr_copy = np.copy(bgr)
-                bgr_copy[top:bot, lft:rig, :] = fusion_bgr
-                return bgr_copy
+                return fusion_bgr
             if 'eyes_center_fix' in sticker:
                 ratio = 1.5
                 h, w, c = bgr.shape
@@ -139,25 +148,25 @@ class LibMasking_Sticker:
                 top = int(max(0, top - hh * ratio))
                 rig = int(min(w, rig + ww * ratio))
                 bot = int(min(h, bot + hh * ratio))
-                dst_h, dst_w = bot - top, rig - lft
                 part = bgr[top:bot, lft:rig, :]
                 cache = XPortrait(part)
                 points_sticker = np.array(sticker['eyes_center_fix'], dtype=np.float32)
-                pts_lft = cache.points[0, 0:1].astype(np.float32)
-                pts_rig = cache.points[0, 1:2].astype(np.float32)
-                points_source = np.concatenate([pts_lft, pts_rig, ], axis=0).astype(np.float32)
+                n = LibMasking_Sticker.getIndexByBox(cache, box)  # default is 0
+                pts_lft = cache.points[n, 0:1].astype(np.float32)
+                pts_rig = cache.points[n, 1:2].astype(np.float32)
+                points_source = np.concatenate([pts_lft, pts_rig], axis=0).astype(np.float32)
+                points_source[:, 0] += lft
+                points_source[:, 1] += top
                 assert len(points_sticker) == len(points_source), (points_sticker.shape, points_source.shape)
-                param = dict(dsize=(dst_w, dst_h), flags=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT, borderValue=(255, 255, 255))
+                param = dict(dsize=(w, h), flags=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT, borderValue=(255, 255, 255))
                 transform = skimage.transform.SimilarityTransform()
                 transform.estimate(points_sticker, points_source)
                 sticker_warped = cv2.warpAffine(sticker_image, transform.params[:2, :], **param)
                 sticker_warped_bgr, sticker_warped_alpha = sticker_warped[:, :, :3], sticker_warped[:, :, 3:4]
                 mask = sticker_warped_alpha.astype(np.float32) / 255.
-                fusion = part * (1 - mask) + sticker_warped_bgr * mask
+                fusion = bgr * (1 - mask) + sticker_warped_bgr * mask
                 fusion_bgr = np.round(fusion).astype(np.uint8)
-                bgr_copy = np.copy(bgr)
-                bgr_copy[top:bot, lft:rig, :] = fusion_bgr
-                return bgr_copy
+                return fusion_bgr
             if 'align' in sticker:
                 ratio = 0.8
                 h, w, c = bgr.shape
@@ -168,21 +177,22 @@ class LibMasking_Sticker:
                 top = int(max(0, top - hh * ratio))
                 rig = int(min(w, rig + ww * ratio))
                 bot = int(min(h, bot + hh * ratio))
-                dst_h, dst_w = bot - top, rig - lft
+                # dst_h, dst_w = bot - top, rig - lft
                 part = bgr[top:bot, lft:rig, :3]
                 cache = XPortrait(part)
-                points_sticker = XPortrait(sticker_image[:, :, :3]).points[0]
-                points_source = cache.points[0]
+                points_sticker = np.copy(XPortrait(sticker_image[:, :, :3]).points[0])
+                n = LibMasking_Sticker.getIndexByBox(cache, box)  # default is 0
+                points_source = np.copy(cache.points[n])
+                points_source[:, 0] += lft
+                points_source[:, 1] += top
                 matrix = cv2.estimateAffinePartial2D(points_sticker, points_source, method=cv2.LMEDS)[0]
-                param = dict(dsize=(dst_w, dst_h), flags=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT, borderValue=(255, 255, 255))
+                param = dict(dsize=(w, h), flags=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT, borderValue=(255, 255, 255))
                 sticker_warped = cv2.warpAffine(sticker_image, matrix, **param)
                 sticker_warped_bgr, sticker_warped_alpha = sticker_warped[:, :, :3], sticker_warped[:, :, 3:4]
                 mask = sticker_warped_alpha.astype(np.float32) / 255.
-                fusion = part * (1 - mask) + sticker_warped_bgr * mask
+                fusion = bgr * (1 - mask) + sticker_warped_bgr * mask
                 fusion_bgr = np.round(fusion).astype(np.uint8)
-                bgr_copy = np.copy(bgr)
-                bgr_copy[top:bot, lft:rig, :] = fusion_bgr
-                return bgr_copy
+                return fusion_bgr
             if 'box' in sticker:
                 H, W, C = bgr.shape
                 box_src, box_fmt = sticker['box']

core/application/face_masking/libscaner.py
@@ -445,7 +445,6 @@ class LibScaner:
             bbox = BoundingBox(np.array([lft, top, rig, bot], dtype=np.int32)).toSquare().clip(0, 0, w, h).asInt()
             if confidence[0] > threshold:
                 return np.array(bbox, dtype=np.int32), 1 + np.mean(1-confidence[5:])
-
             else:
                 return np.array(bbox, dtype=np.int32), 4 + np.mean(1-confidence[5:])
         if confidence[3] > threshold and confidence[1] > threshold:
@@ -454,14 +453,16 @@ class LibScaner:
                 assert confidence[0] > threshold, confidence[0]
                 rig_ratio = float(rig - points[1, 0]) / float(rig - points[0, 0])
                 lft = points[2, 0] - float(rig - points[0, 0]) * (1 - rig_ratio)
-                len_c2rig = rig - points[0, 0]
+                lft, rig = min(lft, rig), max(lft, rig)
+                len_c2rig = abs(rig - points[0, 0])
                 top = int(max(points[0, 1] - 0.4 * len_c2rig, 0))
                 bot = int(min(points[0, 1] + 0.8 * len_c2rig, h))
                 bbox = BoundingBox(np.array([lft, top, rig, bot], dtype=np.int32)).toSquare().clip(0, 0, w, h).asInt()
                 return np.array(bbox, dtype=np.int32), 2 + np.mean(1-confidence[5:])
             if confidence[0] > threshold:
                 lft = points[0, 0] - abs(points[0, 0] - points[1, 0])  # min(lft, points[0, 0])
-                len_c2rig = rig - points[0, 0]
+                lft, rig = min(lft, rig), max(lft, rig)
+                len_c2rig = abs(rig - points[0, 0])
                 top = int(max(points[0, 1] - 0.4 * len_c2rig, 0))
                 bot = int(min(points[0, 1] + 0.8 * len_c2rig, h))
                 bbox = BoundingBox(np.array([lft, top, rig, bot], dtype=np.int32)).toSquare().clip(0, 0, w, h).asInt()
@@ -472,14 +473,16 @@ class LibScaner:
                 assert confidence[0] > threshold, confidence[0]
                 lft_ratio = float(points[2, 0]-lft) / float(points[0, 0]-lft)
                 rig = points[1, 0] + float(points[0, 0] - lft) * (1 - lft_ratio)
-                len_c2lft = points[0, 0] - lft
+                lft, rig = min(lft, rig), max(lft, rig)
+                len_c2lft = abs(points[0, 0] - lft)
                 top = int(max(points[0, 1] - 0.4 * len_c2lft, 0))
                 bot = int(min(points[0, 1] + 0.8 * len_c2lft, h))
                 bbox = BoundingBox(np.array([lft, top, rig, bot], dtype=np.int32)).toSquare().clip(0, 0, w, h).asInt()
                 return np.array(bbox, dtype=np.int32), 2 + np.mean(1-confidence[5:])
             if confidence[0] > threshold:
                 rig = points[0, 0] + abs(points[2, 0] - points[0, 0])  # min(lft, points[0, 0])
-                len_c2lft = points[0, 0] - lft
+                lft, rig = min(lft, rig), max(lft, rig)
+                len_c2lft = abs(points[0, 0] - lft)
                 top = int(max(points[0, 1] - 0.4 * len_c2lft, 0))
                 bot = int(min(points[0, 1] + 0.8 * len_c2lft, h))
                 bbox = BoundingBox(np.array([lft, top, rig, bot], dtype=np.int32)).toSquare().clip(0, 0, w, h).asInt()

core/base/cache/xportrait_helper.py
@@ -26,12 +26,26 @@ class XPortraitHelper:
     @staticmethod
     def getAjna(cache):
         assert isinstance(cache, XPortrait), type(cache)
-        return [np.mean(cache.landmark[n][17:27, :], axis=0) for n in range(cache.number)]
+        ajna_list = []
+        for n in range(cache.number):
+            ajna_list.append(np.mean(cache.landmark[n][17:27, :], axis=0))
+        return np.reshape(np.array(ajna_list, dtype=np.float32), (cache.number, 2))  # N,2
 
     @staticmethod
     def getCenterOfEyes(cache):
         assert isinstance(cache, XPortrait), type(cache)
-        return [np.mean(cache.landmark[n][36:48, :], axis=0) for n in range(cache.number)]
+        center_both_eyes_list = []
+        for n in range(cache.number):
+            center_both_eyes_list.append(np.mean(cache.landmark[n][36:48, :], axis=0))
+        return np.reshape(np.array(center_both_eyes_list, dtype=np.float32), (cache.number, 2))  # N,2
+
+    @staticmethod
+    def getCenterOfEachEyes(cache):
+        assert isinstance(cache, XPortrait), type(cache)
+        center_each_eyes_list = []
+        for n in range(cache.number):
+            center_each_eyes_list.append((np.mean(cache.landmark[n][36:40, :], axis=0), np.mean(cache.landmark[n][42:48, :], axis=0)))
+        return np.reshape(np.array(center_each_eyes_list, dtype=np.float32), (cache.number, 2, 2))  # N,2,2
 
     @staticmethod
     def getFaceRegion(cache, index=None, top_line='brow', value=255):