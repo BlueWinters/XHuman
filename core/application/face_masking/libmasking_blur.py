update application.masking -- 1.fix bug for scanner 2.optimize mosaic result 3.optimize multi-faces masking with instance segmentation

core/application/face_masking/libmasking_blur.py
@@ -10,7 +10,7 @@ import typing
 import tqdm
 # import numba
 from .boundingbox import Rectangle, BoundingBox
-from .morphology import getMaxRegion
+from .helper.image_helper import MaskingHelper
 from ...base import XPortrait, XPortraitHelper
 from ...utils.context import XContextTimer
 from ...utils.video import XVideoReader, XVideoWriter
@@ -202,31 +202,7 @@ class LibMasking_Blur:
 
     @staticmethod
     def getHeadMask(bgr, box, value=255):
-        lft, top, rig, bot = box
-        cache = XPortrait(bgr[top:bot, lft:rig, :])
-        if cache.number == 1:
-            parsing = cache.parsing
-            mask = np.where((0 < parsing) & (parsing < 15) & (parsing != 12), value, 0).astype(np.uint8)
-            return getMaxRegion(mask) * value
-        if cache.number > 1:
-            box_src = np.reshape(np.array(box, dtype=np.int32), (1, 4))
-            box_cur = np.reshape(np.array(cache.box, dtype=np.int32), (-1, 4))
-            iou = BoundingBox.computeIOU(boxes1=box_src, boxes2=box_cur)  # 1,N
-            part_copy = np.copy(cache.bgr)
-            selected_index = int(np.argmax(iou[0, :]))
-            for n in range(cache.number):
-                if n != selected_index:
-                    l, t, r, b = cache.box[n, :]
-                    part_copy[t:b, l:r, :] = 255
-            l, t, r, b = cache.box[selected_index, :]
-            part_copy[t:b, l:r, :] = cache.bgr[t:b, l:r, :]
-            parsing = XPortrait(part_copy).parsing
-            mask = np.where((0 < parsing) & (parsing < 15) & (parsing != 12), value, 0).astype(np.uint8)
-            return getMaxRegion(mask) * value
-        # note: detect face fail
-        parsing = cache.parsing
-        mask = np.where((0 < parsing) & (parsing < 15) & (parsing != 12), value, 0).astype(np.uint8)
-        return getMaxRegion(mask) * value
+        return MaskingHelper.getHeadMaskByParsing(bgr, box, value)
 
     @staticmethod
     def workOnSelected(source_bgr, blured_bgr, kernel=17, mask=None, **kwargs):

core/application/face_masking/libmasking_mosaic.py
@@ -6,7 +6,7 @@ import numpy as np
 import tqdm
 from skimage import segmentation
 from .boundingbox import Rectangle, BoundingBox
-from .morphology import getMaxRegion
+from .helper.image_helper import MaskingHelper
 from ...base.cache import XPortrait, XPortraitHelper
 from ...thirdparty.cache import XBody
 from ...utils.context import XContextTimer
@@ -94,7 +94,7 @@ class LibMasking_Mosaic:
             cache = XPortrait(bgr)
             h, w = cache.shape
             # lft, top, rig, bot = box
-            lft, top, rig, bot = Rectangle(box).toSquare().expand(0.5, 0.5).clip(0, 0, w, h).asInt()
+            lft, top, rig, bot = Rectangle(box).toSquare().expand(0.8, 0.8).clip(0, 0, w, h).asInt()
             hh = bot - top
             ww = rig - lft
             nh = int(float(h / hh) * num_pixels)
@@ -115,12 +115,14 @@ class LibMasking_Mosaic:
     """
     @staticmethod
     def doSuperPixel(bgr, **kwargs):
+        kernel = 11
+        bgr = cv2.GaussianBlur(bgr, (kernel, kernel), sigmaX=kernel // 2, sigmaY=kernel // 2)
         n_div = kwargs.pop('n_div', 32)
         mesh_size = kwargs.pop('mesh_size', 32)
         n_div = n_div if n_div > 0 else int((bgr.shape[0] * bgr.shape[1]) / (mesh_size * mesh_size))
         h, w, c = bgr.shape
         n_segments = n_div * int(n_div * max(h, w) / min(h, w))
-        compactness = kwargs.pop('compactness', 40)
+        compactness = kwargs.pop('compactness', 10)
         mask = kwargs['mask'] if 'mask' in kwargs else None
         segments = segmentation.slic(bgr, n_segments=n_segments, slic_zero=True, compactness=compactness, start_label=1, mask=mask)
         # segments = cv2.resize(segments, (w // 2, h // 2), interpolation=cv2.INTER_NEAREST)
@@ -146,8 +148,14 @@ class LibMasking_Mosaic:
         bgr_copy = np.copy(bgr)
         bgr_copy = LibMasking_Mosaic.visualAsMean(seg, bgr_copy, 0)
         if vis_boundary is True:
-            bgr_copy = segmentation.mark_boundaries(bgr_copy, seg, color=(1, 1, 1), mode='outer')
-            bgr_copy = np.round(bgr_copy * 255).astype(np.uint8)
+            # bgr_copy = segmentation.mark_boundaries(bgr_copy, seg, color=(1, 1, 1), mode='outer')
+            # bgr_copy = np.round(bgr_copy * 255).astype(np.uint8)
+            # TODO: debug
+            boundary = segmentation.find_boundaries(seg, mode='thick').astype(np.uint8) * 255
+            boundary = cv2.resize(boundary, bgr.shape[:2][::-1], interpolation=cv2.INTER_NEAREST)
+            multi = boundary.astype(np.float32)[:, :, None] / 255.
+            fusion = bgr_copy.astype(np.float32) * (1-multi) + np.ones_like(bgr_copy) * 255 * multi
+            bgr_copy = np.round(fusion).astype(np.uint8)
         return bgr_copy
 
     @staticmethod
@@ -174,30 +182,7 @@ class LibMasking_Mosaic:
 
     @staticmethod
     def getHeadMask(bgr, box, value=255):
-        lft, top, rig, bot = box
-        cache = XPortrait(bgr[top:bot, lft:rig, :])
-        if cache.number == 1:
-            parsing = cache.parsing
-            mask = np.where((0 < parsing) & (parsing < 15) & (parsing != 12), value, 0).astype(np.uint8)
-            return getMaxRegion(mask) * value
-        if cache.number > 1:
-            box_src = np.reshape(np.array(box, dtype=np.int32), (1, 4))
-            box_cur = np.reshape(np.array(cache.box, dtype=np.int32), (-1, 4))
-            iou = BoundingBox.computeIOU(boxes1=box_src, boxes2=box_cur)  # 1,N
-            part_copy = np.copy(cache.bgr)
-            selected_index = int(np.argmax(iou[0, :]))
-            for n in range(cache.number):
-                if n != selected_index:
-                    l, t, r, b = cache.box[n, :]
-                    part_copy[t:b, l:r, :] = 255
-            part_copy[top:bot, lft:rig, :] = bgr[top:bot, lft:rig, :]
-            parsing = XPortrait(part_copy).parsing
-            mask = np.where((0 < parsing) & (parsing < 15) & (parsing != 12), value, 0).astype(np.uint8)
-            return getMaxRegion(mask) * value
-        # note: detect face fail
-        parsing = cache.parsing
-        mask = np.where((0 < parsing) & (parsing < 15) & (parsing != 12), value, 0).astype(np.uint8)
-        return getMaxRegion(mask) * value
+        return MaskingHelper.getHeadMaskByParsing(bgr, box, value)
 
     @staticmethod
     def getMaskFromBox(h, w, box, ratio, value=255):
@@ -217,7 +202,7 @@ class LibMasking_Mosaic:
     def inferenceBoxWithMosaicPolygon(bgr, box, n_div, vis_boundary, focus_type, super_pixels=None):
         if focus_type == 'head' or focus_type == 'face':
             h, w, c = bgr.shape
-            lft, top, rig, bot = Rectangle(box).toSquare().expand(0.5, 0.5).clip(0, 0, w, h).asInt()
+            lft, top, rig, bot = Rectangle(box).toSquare().expand(0.8, 0.8).clip(0, 0, w, h).asInt()
             part = bgr[top:bot, lft:rig]
             size = float(sum(part.shape[:2])) / 2  # (h+w)/2
             # n_div = int(size / 256 * n_div)

core/application/face_masking/libscaner.py
@@ -8,6 +8,7 @@ import numpy as np
 import dataclasses
 import tqdm
 import json
+import skimage
 from .boundingbox import BoundingBox
 from ...base.cache import XPortrait, XPortraitHelper
 from ...thirdparty.cache import XBody, XBodyHelper
@@ -429,6 +430,24 @@ class LibScaner:
         return -1
 
     @staticmethod
+    def realignFace(points, w, h, index):
+        template = np.array([[192, 239], [318, 240], [256, 314]], dtype=np.float32)
+        dst_pts = points[np.array(index, dtype=np.int32)]
+        src_pts = template[:len(dst_pts), :]
+        transform = skimage.transform.SimilarityTransform()
+        transform.estimate(src_pts, dst_pts)
+        box = np.array([[0, 0, 1], [512, 0, 1], [512, 512, 1], [0, 512, 1]], dtype=np.float32)
+        box_remap = np.dot(transform.params, box.T)[:2, :].T
+        box_remap_int = box_remap.astype(np.int32)
+        lft = np.min(box_remap_int[:, 0])
+        rig = np.max(box_remap_int[:, 0])
+        top = np.min(box_remap_int[:, 1])
+        bot = np.max(box_remap_int[:, 1])
+        # bbox = lft, top, rig, bot
+        bbox = BoundingBox(np.array([lft, top, rig, bot], dtype=np.int32)).toSquare().clip(0, 0, w-1, h-1).asInt()
+        return bbox
+
+    @staticmethod
     def transformPoints2FaceBox(bgr, key_points, box, threshold=0.5):
         h, w, c = bgr.shape
         confidence = key_points[:, 2].astype(np.float32)
@@ -442,50 +461,67 @@ class LibScaner:
             ctr_ear = (lft_ear + rig_ear) / 2
             top = int(max(ctr_ear[1] - 0.4 * len_ear, 0))
             bot = int(min(ctr_ear[1] + 0.6 * len_ear, h))
-            bbox = BoundingBox(np.array([lft, top, rig, bot], dtype=np.int32)).toSquare().clip(0, 0, w, h).asInt()
-            if confidence[0] > threshold:
-                return np.array(bbox, dtype=np.int32), 1 + np.mean(1-confidence[5:])
+            if points[4, 0] < points[2, 0] < points[0, 0] < points[1, 0] < points[3, 0]:
+                bbox = BoundingBox(np.array([lft, top, rig, bot], dtype=np.int32)).toSquare().clip(0, 0, w-1, h-1).asInt()
+                if confidence[0] > threshold:
+                    return np.array(bbox, dtype=np.int32), 1 + np.mean(1-confidence[5:])
+                else:
+                    return np.array(bbox, dtype=np.int32), 4 + np.mean(1-confidence[5:])
             else:
-                return np.array(bbox, dtype=np.int32), 4 + np.mean(1-confidence[5:])
+                if confidence[1] > threshold and confidence[2] > threshold:
+                    bbox = LibScaner.realignFace(points, w, h, index=[2, 1, 0])
+                    return np.array(bbox, dtype=np.int32), 4 + np.mean(1 - confidence[5:])
         if confidence[3] > threshold and confidence[1] > threshold:
             rig = points[3, 0]  # points[1, 0] < points[3, 0]
             if confidence[2] > threshold:
                 assert confidence[0] > threshold, confidence[0]
-                rig_ratio = float(rig - points[1, 0]) / float(rig - points[0, 0])
-                lft = points[2, 0] - float(rig - points[0, 0]) * (1 - rig_ratio)
-                lft, rig = min(lft, rig), max(lft, rig)
-                len_c2rig = abs(rig - points[0, 0])
-                top = int(max(points[0, 1] - 0.4 * len_c2rig, 0))
-                bot = int(min(points[0, 1] + 0.8 * len_c2rig, h))
-                bbox = BoundingBox(np.array([lft, top, rig, bot], dtype=np.int32)).toSquare().clip(0, 0, w, h).asInt()
+                if points[2, 0] < points[0, 0] < points[1, 0] < points[3, 0]:
+                    rig_ratio = float(rig - points[1, 0]) / float(rig - points[0, 0])
+                    lft = points[2, 0] - float(rig - points[0, 0]) * (1 - rig_ratio)
+                    lft, rig = min(lft, rig), max(lft, rig)
+                    len_c2rig = abs(rig - points[0, 0])
+                    top = int(max(points[0, 1] - 0.4 * len_c2rig, 0))
+                    bot = int(min(points[0, 1] + 0.8 * len_c2rig, h))
+                    bbox = BoundingBox(np.array([lft, top, rig, bot], dtype=np.int32)).toSquare().clip(0, 0, w-1, h-1).asInt()
+                else:
+                    bbox = LibScaner.realignFace(points, w, h, index=[2, 1, 0])
                 return np.array(bbox, dtype=np.int32), 2 + np.mean(1-confidence[5:])
             if confidence[0] > threshold:
-                lft = points[0, 0] - abs(points[0, 0] - points[1, 0])  # min(lft, points[0, 0])
-                lft, rig = min(lft, rig), max(lft, rig)
-                len_c2rig = abs(rig - points[0, 0])
-                top = int(max(points[0, 1] - 0.4 * len_c2rig, 0))
-                bot = int(min(points[0, 1] + 0.8 * len_c2rig, h))
-                bbox = BoundingBox(np.array([lft, top, rig, bot], dtype=np.int32)).toSquare().clip(0, 0, w, h).asInt()
+                if points[0, 0] < points[1, 0] < points[3, 0]:
+                    lft = points[0, 0] - abs(points[0, 0] - points[1, 0])  # min(lft, points[0, 0])
+                    lft, rig = min(lft, rig), max(lft, rig)
+                    len_c2rig = abs(rig - points[0, 0])
+                    top = int(max(points[0, 1] - 0.4 * len_c2rig, 0))
+                    bot = int(min(points[0, 1] + 0.8 * len_c2rig, h))
+                    bbox = BoundingBox(np.array([lft, top, rig, bot], dtype=np.int32)).toSquare().clip(0, 0, w-1, h-1).asInt()
+                else:
+                    bbox = LibScaner.realignFace(points, w, h, index=[1, 0])
                 return np.array(bbox, dtype=np.int32), 3 + np.mean(1-confidence[5:])
         if confidence[4] > threshold and confidence[2] > threshold:
             lft = points[4, 0]
             if confidence[1] > threshold:
                 assert confidence[0] > threshold, confidence[0]
-                lft_ratio = float(points[2, 0]-lft) / float(points[0, 0]-lft)
-                rig = points[1, 0] + float(points[0, 0] - lft) * (1 - lft_ratio)
-                lft, rig = min(lft, rig), max(lft, rig)
-                len_c2lft = abs(points[0, 0] - lft)
-                top = int(max(points[0, 1] - 0.4 * len_c2lft, 0))
-                bot = int(min(points[0, 1] + 0.8 * len_c2lft, h))
-                bbox = BoundingBox(np.array([lft, top, rig, bot], dtype=np.int32)).toSquare().clip(0, 0, w, h).asInt()
+                if points[4, 0] < points[2, 0] < points[0, 0] < points[1, 0]:
+                    lft_ratio = float(points[2, 0]-lft) / float(points[0, 0]-lft)
+                    rig = points[1, 0] + float(points[0, 0] - lft) * (1 - lft_ratio)
+                    lft, rig = min(lft, rig), max(lft, rig)
+                    len_c2lft = abs(points[0, 0] - lft)
+                    top = int(max(points[0, 1] - 0.4 * len_c2lft, 0))
+                    bot = int(min(points[0, 1] + 0.8 * len_c2lft, h))
+                    bbox = BoundingBox(np.array([lft, top, rig, bot], dtype=np.int32)).toSquare().clip(0, 0, w-1, h-1).asInt()
+                else:
+                    bbox = LibScaner.realignFace(points, w, h, index=[2, 1, 0])
                 return np.array(bbox, dtype=np.int32), 2 + np.mean(1-confidence[5:])
             if confidence[0] > threshold:
-                rig = points[0, 0] + abs(points[2, 0] - points[0, 0])  # min(lft, points[0, 0])
-                lft, rig = min(lft, rig), max(lft, rig)
-                len_c2lft = abs(points[0, 0] - lft)
-                top = int(max(points[0, 1] - 0.4 * len_c2lft, 0))
-                bot = int(min(points[0, 1] + 0.8 * len_c2lft, h))
-                bbox = BoundingBox(np.array([lft, top, rig, bot], dtype=np.int32)).toSquare().clip(0, 0, w, h).asInt()
+                if points[4, 0] < points[2, 0] < points[0, 0]:
+                    rig = points[0, 0] + abs(points[2, 0] - points[0, 0])  # min(lft, points[0, 0])
+                    lft, rig = min(lft, rig), max(lft, rig)
+                    len_c2lft = abs(points[0, 0] - lft)
+                    top = int(max(points[0, 1] - 0.4 * len_c2lft, 0))
+                    bot = int(min(points[0, 1] + 0.8 * len_c2lft, h))
+                    bbox = BoundingBox(np.array([lft, top, rig, bot], dtype=np.int32)).toSquare().clip(0, 0, w-1, h-1).asInt()
+                else:
+                    bbox = LibScaner.realignFace(points, w, h, index=[2, 0])
                 return np.array(bbox, dtype=np.int32), 3 + np.mean(1-confidence[5:])
         return np.array([0, 0, 0, 0], dtype=np.int32), 4 + np.mean(1-confidence[5:])
 