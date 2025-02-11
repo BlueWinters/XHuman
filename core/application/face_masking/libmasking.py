update application.masking -- fix bug of rendering and task-2332

core/application/face_masking/libmasking.py
@@ -28,10 +28,10 @@ class LibMasking:
     def benchmarkOnImage():
         from core.thirdparty.cartoon.libcartoon import LibCartoonWrapperQ
         # N:\archive\2024\1126-video\DanceShow2\01\test-image
-        path_in_image = R'N:\archive\2024\1126-video\error\image\12.png'
-        path_out_json = R'N:\archive\2024\1126-video\error\image\12.json'
-        path_out_image = R'N:\archive\2024\1126-video\error\image\12-output.png'
-        path_visual = R'N:\archive\2024\1126-video\error\image\12-visual.png'
+        path_in_image = R'N:\archive\2024\1126-video\error\image\10.png'
+        path_out_json = R'N:\archive\2024\1126-video\error\image\10.json'
+        path_out_image = R'N:\archive\2024\1126-video\error\image\10-output.png'
+        path_visual = R'N:\archive\2024\1126-video\error\image\10-visual.png'
         video_info, visual_bgr = LibMasking.scanningImage(path_in_image, path_out_json=path_out_json, visual_scanning=True)
         cv2.imwrite(path_visual, visual_bgr)
         # video_info = VideoInfo.loadVideoInfo(path_in_json=path_out_json)
@@ -49,16 +49,20 @@ class LibMasking:
         # options_dict[2] = MaskingOption(301, dict(bgr=sticker, eyes_center_fix=[[397,470], [593,470]]))
         # options_dict[1] = MaskingOption(101, dict(blur_type='blur_gaussian', focus_type='head'))
         # options_dict[2] = MaskingOption(202, dict(mosaic_type='mosaic_pixel_polygon_small', focus_type='head'))
-        # options_dict[1] = MaskingOption(101, dict(blur_type='blur_gaussian', focus_type='head'))
-        # options_dict[3] = MaskingOption(301, dict(bgr=sticker1, eyes_center=[[397, 470], [593, 470]]))
-        # options_dict[4] = MaskingOption(301, dict(bgr=sticker3, eyes_center=[[427, 438], [543, 438]]))
-        # options_dict[5] = MaskingOption(301, dict(bgr=sticker2, eyes_center=[[857, 1735], [1683, 1735]]))
-        # options_dict[6] = MaskingOption(202, dict(mosaic_type='mosaic_pixel_polygon_small_line', focus_type='head'))
-        options_dict[1] = MaskingOption(202, dict(mosaic_type='mosaic_pixel_polygon_big_line', focus_type='head'))
-        options_dict[2] = MaskingOption(202, dict(mosaic_type='mosaic_pixel_polygon_big_line', focus_type='head'))
-        options_dict[3] = MaskingOption(202, dict(mosaic_type='mosaic_pixel_polygon_big_line', focus_type='head'))
+        options_dict[1] = MaskingOption(101, dict(blur_type='blur_gaussian', focus_type='head'))
+        options_dict[2] = MaskingOption(301, dict(bgr=sticker1, eyes_center=[[397, 470], [593, 470]]))
+        options_dict[3] = MaskingOption(301, dict(bgr=sticker3, eyes_center=[[427, 438], [543, 438]]))
+        options_dict[4] = MaskingOption(202, dict(mosaic_type='mosaic_pixel_polygon_big', focus_type='head'))
+        options_dict[5] = MaskingOption(301, dict(bgr=sticker2, eyes_center=[[857, 1735], [1683, 1735]]))
+        options_dict[6] = MaskingOption(202, dict(mosaic_type='mosaic_pixel_polygon_small_line', focus_type='head'))
+        # options_dict[1] = MaskingOption(202, dict(mosaic_type='mosaic_pixel_polygon_small', focus_type='head'))
+        # options_dict[2] = MaskingOption(202, dict(mosaic_type='mosaic_pixel_polygon_small', focus_type='head'))
+        # options_dict[3] = MaskingOption(202, dict(mosaic_type='mosaic_pixel_polygon_small', focus_type='head'))
         # options_dict[4] = MaskingOption(202, dict(mosaic_type='mosaic_pixel_polygon_small_line', focus_type='head'))
-
+        # options_dict[1] = MaskingOption(101, dict(blur_type='blur_gaussian', focus_type='head'))
+        # options_dict[2] = MaskingOption(101, dict(blur_type='blur_motion', focus_type='head'))
+        # options_dict[3] = MaskingOption(101, dict(blur_type='blur_water', focus_type='head'))
+        # options_dict[4] = MaskingOption(101, dict(blur_type='blur_diffuse', focus_type='head'))
         result = LibMasking.maskingImage(path_in_image, options_dict, path_in_json=path_out_json)
         cv2.imwrite(path_out_image, result)
 
@@ -86,27 +90,28 @@ class LibMasking:
         # path_out_video_scanning = R'N:\archive\2024\1126-video\error\2\input-scanning.mp4'
         # path_out_video_masking = R'N:\archive\2024\1126-video\error\2\input-masking-blur.mp4'
 
-        path_in_video = R'N:\archive\2024\1126-video\error\11\input.mp4'
-        path_out_json = R'N:\archive\2024\1126-video\error\11\input.json'
-        path_out_video_scanning = R'N:\archive\2024\1126-video\error\11\input-scanning.mp4'
-        path_out_video_masking = R'N:\archive\2024\1126-video\error\11\input-masking-blur.mp4'
+        path_in_video = R'N:\archive\2024\1126-video\error\6\input.mp4'
+        path_out_json = R'N:\archive\2024\1126-video\error\6\input.json'
+        path_out_video_scanning = R'N:\archive\2024\1126-video\error\6\input-scanning.mp4'
+        path_out_video_masking = R'N:\archive\2024\1126-video\error\6\input-masking-blur.mp4'
 
         # pipeline
         video_info = LibMasking.scanningVideo(path_in_video, path_out_json=path_out_json, path_out_video=path_out_video_scanning)
+
         # video_info = VideoInfo.loadVideoInfo(path_in_json=path_out_json)
-        options_dict = MaskingOption.getRandomMaskingOptionDict(video_info.person_identity_history)
+        # options_dict = MaskingOption.getRandomMaskingOptionDict(video_info.person_identity_history)
         # options_dict = dict()
-        # for identity, data in video_info.getIdentityPreviewDict().items():
+        for identity, data in video_info.getIdentityPreviewDict().items():
         #     bgr = data['image']
         #     box = data['box']
-        #     cv2.imwrite(R'N:\archive\2024\1126-video\error\preview-{}.png'.format(identity), data['face'])
+            cv2.imwrite(R'N:\archive\2024\1126-video\error\6\preview-{}.png'.format(identity), data['face'])
         #     # try:
         #     bgr_style, crop_box = LibCartoonWrapperQ.inference(np.copy(bgr), np.array(box, dtype=np.int32).tolist())
         #     options_dict[identity] = MaskingOption(301, dict(bgr=bgr_style, box=crop_box))
         #     cv2.imwrite(R'N:\archive\2024\1126-video\error\cartoon-{}.png'.format(identity), bgr_style)
         #     # except:
         #     #     pass
-        LibMasking.maskingVideo(path_in_video, options_dict, path_out_video_masking, path_in_json=path_out_json, debug_mode=True)
+        # LibMasking.maskingVideo(path_in_video, options_dict, path_out_video_masking, path_in_json=path_out_json, debug_mode=True)
         # LibMasking.maskingVideoOld(path_in_video, options_dict, path_out_video_masking, path_in_json=path_out_json)
 
         # path_in_video = R'N:\archive\2024\1126-video\DanceShow4\input.mp4'
@@ -154,13 +159,13 @@ class LibMasking:
     """
     """
     @staticmethod
-    def maskingSingleFace(index_frame, bgr, box, masking_option: MaskingOption):
+    def maskingSingleFace(index_frame, bgr, canvas, box, masking_option: MaskingOption):
         if masking_option.option_code in MaskingOption.MaskingOption_Blur:
-            return LibMasking_Blur.inferenceWithBox(bgr, box, masking_option)
+            return LibMasking_Blur.inferenceWithBox(bgr, canvas, box, masking_option)
         if masking_option.option_code in MaskingOption.MaskingOption_Mosaic:
-            return LibMasking_Mosaic.inferenceWithBox(bgr, box, masking_option)
+            return LibMasking_Mosaic.inferenceWithBox(bgr, canvas, box, masking_option)
         if masking_option.option_code in MaskingOption.MaskingOption_Sticker:
-            return LibMasking_Sticker.inferenceWithBox(bgr, box, masking_option)
+            return LibMasking_Sticker.inferenceWithBox(bgr, canvas, box, masking_option)
         raise NotImplementedError(masking_option)
 
     """
@@ -208,12 +213,13 @@ class LibMasking:
         with XContextTimer(True):
             with tqdm.tqdm(total=len(reader)) as bar:
                 for index_frame, bgr in enumerate(reader):
+                    canvas = np.copy(bgr)
                     for _, (person, it) in enumerate(iterator_list):
                         info: PersonFrameInfo = it.next()
                         if info.index_frame == index_frame:
                             if person.identity in options_dict:
                                 masking_option = options_dict[person.identity]
-                                bgr = LibMasking.maskingSingleFace(index_frame, bgr, info.box_face, masking_option)
+                                bgr = LibMasking.maskingSingleFace(index_frame, bgr, canvas, info.box_face, masking_option)
                                 # if np.sum(info.box_face) == 0:
                                 #     bgr = LibMasking.maskingSingleFace(index_frame, bgr, it.previous().box_face, masking_option)
                                 # else:
@@ -275,11 +281,12 @@ class LibMasking:
             options_dict = MaskingOption.getRandomMaskingOptionDict(video_info.person_identity_history)
         iterator_list = [(person, person.getInfoIterator()) for person in video_info.person_identity_history]
         bgr = cv2.imread(path_image_or_bgr) if isinstance(path_image_or_bgr, str) else np.array(path_image_or_bgr, dtype=np.uint8)
+        canvas = np.copy(bgr)
         for n, (person, it) in enumerate(iterator_list):
             info: PersonFrameInfo = it.next()
             if person.identity in options_dict:
                 masking_option = options_dict[person.identity]
-                bgr = LibMasking.maskingSingleFace(0, bgr, info.box_face, masking_option)
+                canvas = LibMasking.maskingSingleFace(n, bgr, canvas, info.box_face, masking_option)
                 schedule_call('打码图片', float((n + 1) / len(iterator_list)))
-        return bgr
+        return canvas
 

core/application/face_masking/libmasking_blur.py
@@ -214,36 +214,36 @@ class LibMasking_Blur:
         return np.round(fusion).astype(np.uint8)
 
     @staticmethod
-    def inferenceWithBox(bgr, box, masking_option):
+    def inferenceWithBox(bgr, canvas, box, masking_option):
         parameters = masking_option.parameters
         blur_type = parameters['blur_type']
         focus_type = parameters['focus_type']
         if blur_type == 'blur_gaussian':
             blur_kernel = parameters['blur_kernel'] if 'blur_kernel' in parameters else 15
-            return LibMasking_Blur.inferenceOnBox_WithBlurGaussian(bgr, box, blur_kernel, focus_type=focus_type)
+            return LibMasking_Blur.inferenceOnBox_WithBlurGaussian(bgr, canvas, box, blur_kernel, focus_type=focus_type)
         if blur_type == 'blur_motion':
             blur_kernel = parameters['blur_kernel'] if 'blur_kernel' in parameters else 15
-            return LibMasking_Blur.inferenceOnBox_WithBlurMotion(bgr, box, blur_kernel, focus_type=focus_type)
+            return LibMasking_Blur.inferenceOnBox_WithBlurMotion(bgr, canvas, box, blur_kernel, focus_type=focus_type)
         if blur_type == 'blur_water':
             rotation_degree = parameters['rotation_degree'] if 'rotation_degree' in parameters else 2
             water_length = parameters['water_length'] if 'water_length' in parameters else 8
             map_coordinates = parameters['map_coordinates'] if 'map_coordinates' in parameters else None
             blur_bgr, map_coordinates = LibMasking_Blur.inferenceOnBox_WithBlurWater(
-                bgr, box, rotation_degree, water_length, focus_type=focus_type, map_coordinates=map_coordinates)
+                bgr, canvas, box, rotation_degree, water_length, focus_type=focus_type, map_coordinates=map_coordinates)
             parameters['map_coordinates'] = map_coordinates
             return blur_bgr
         if blur_type == 'blur_pencil':
             k_neigh = parameters['k_neigh'] if 'k_neigh' in parameters else 17
             pre_k = parameters['pre_k'] if 'pre_k' in parameters else 3
             post_k = parameters['post_k'] if 'post_k' in parameters else 3
-            return LibMasking_Blur.inferenceOnBox_WithBlurDiffusion(bgr, box, k_neigh, pre_k, post_k, focus_type=focus_type)
+            return LibMasking_Blur.inferenceOnBox_WithBlurDiffusion(bgr, canvas, box, k_neigh, pre_k, post_k, focus_type=focus_type)
         if blur_type == 'blur_diffuse':
             k_neigh = parameters['k_neigh'] if 'k_neigh' in parameters else 17
-            return LibMasking_Blur.inferenceOnBox_WithBlurDiffusion(bgr, box, k_neigh, 0, 0, focus_type=focus_type)
+            return LibMasking_Blur.inferenceOnBox_WithBlurDiffusion(bgr, canvas, box, k_neigh, 0, 0, focus_type=focus_type)
         raise NotImplementedError(parameters)
 
     @staticmethod
-    def inferenceOnBox_WithBlurGaussian(bgr, box, blur_kernel, focus_type):
+    def inferenceOnBox_WithBlurGaussian(bgr, canvas, box, blur_kernel, focus_type):
         h, w = bgr.shape[:2]
         blured_bgr = LibMasking_Blur.doWithGaussianBlur(bgr, kernel=blur_kernel)
         if focus_type == 'face':
@@ -258,10 +258,10 @@ class LibMasking_Blur:
             mask[box[3]:, :] = 0
         else:
             mask = LibMasking_Blur.getMaskFromBox(h, w, box, ratio=0.)
-        return LibMasking_Blur.workOnSelected(bgr, blured_bgr, mask=mask)
+        return LibMasking_Blur.workOnSelected(canvas, blured_bgr, mask=mask)
 
     @staticmethod
-    def inferenceOnBox_WithBlurMotion(bgr, box, blur_kernel, focus_type):
+    def inferenceOnBox_WithBlurMotion(bgr, canvas, box, blur_kernel, focus_type):
         h, w = bgr.shape[:2]
         blured_bgr = LibMasking_Blur.doWithBlurMotion(bgr, kernel=blur_kernel)
         if focus_type == 'face':
@@ -276,10 +276,10 @@ class LibMasking_Blur:
             mask[box[3]:, :] = 0
         else:
             mask = LibMasking_Blur.getMaskFromBox(h, w, box, ratio=0.)
-        return LibMasking_Blur.workOnSelected(bgr, blured_bgr, mask=mask)
+        return LibMasking_Blur.workOnSelected(canvas, blured_bgr, mask=mask)
 
     @staticmethod
-    def inferenceOnBox_WithBlurWater(bgr, box, a, b, focus_type, map_coordinates=None):
+    def inferenceOnBox_WithBlurWater(bgr, canvas, box, a, b, focus_type, map_coordinates=None):
         h, w = bgr.shape[:2]
         lft, top, rig, bot = box
         if focus_type == 'face':
@@ -299,10 +299,10 @@ class LibMasking_Blur:
         blured_bgr = cv2.resize(blured_bgr, part.shape[:2][::-1])
         copy_bgr = np.copy(bgr)
         copy_bgr[top:bot, lft:rig, :] = blured_bgr
-        return LibMasking_Blur.workOnSelected(bgr, copy_bgr, mask=mask), map_coordinates
+        return LibMasking_Blur.workOnSelected(canvas, copy_bgr, mask=mask), map_coordinates
 
     @staticmethod
-    def inferenceOnBox_WithBlurDiffusion(bgr, box, k_neigh, pre_k, post_k, focus_type):
+    def inferenceOnBox_WithBlurDiffusion(bgr, canvas, box, k_neigh, pre_k, post_k, focus_type):
         h, w = bgr.shape[:2]
         blured_bgr = LibMasking_Blur.doWithBlurDiffusion(bgr, k_neigh, pre_k, post_k)
         if focus_type == 'face':
@@ -317,7 +317,7 @@ class LibMasking_Blur:
             mask[box[3]:, :] = 0
         else:
             mask = LibMasking_Blur.getMaskFromBox(h, w, box, ratio=0.)
-        return LibMasking_Blur.workOnSelected(bgr, blured_bgr, mask=mask)
+        return LibMasking_Blur.workOnSelected(canvas, blured_bgr, mask=mask)
 
     """
     """

core/application/face_masking/libmasking_mosaic.py
@@ -89,7 +89,7 @@ class LibMasking_Mosaic:
         return bgr_copy
 
     @staticmethod
-    def inferenceBoxWithMosaicSquare(bgr, box, num_pixels, focus_type):
+    def inferenceBoxWithMosaicSquare(bgr, canvas, box, num_pixels, focus_type):
         if focus_type == 'head' or focus_type == 'face':
             cache = XPortrait(bgr)
             h, w = cache.shape
@@ -107,7 +107,7 @@ class LibMasking_Mosaic:
             if focus_type == 'head':
                 mask[top:bot, lft:rig] = LibMasking_Mosaic.getHeadMask(bgr, box_src=box, box_tar=(lft, top, rig, bot))
                 mask[box[3]:, :] = 0
-            return LibMasking_Mosaic.workOnSelected(cache.bgr, mosaic_bgr, mask=mask)
+            return LibMasking_Mosaic.workOnSelected(canvas, mosaic_bgr, mask=mask)
         else:
             return LibMasking_Mosaic.resizeDownAndUp(bgr, box, num_pixels, num_pixels)
 
@@ -199,7 +199,7 @@ class LibMasking_Mosaic:
         return mask
 
     @staticmethod
-    def inferenceBoxWithMosaicPolygon(bgr, box, n_div, vis_boundary, focus_type, super_pixels=None):
+    def inferenceBoxWithMosaicPolygon(bgr, canvas, box, n_div, vis_boundary, focus_type, super_pixels=None):
         if focus_type == 'head' or focus_type == 'face':
             h, w, c = bgr.shape
             lft, top, rig, bot = Rectangle(box).toSquare().expand(0.8, 0.8).clip(0, 0, w, h).asInt()
@@ -220,7 +220,7 @@ class LibMasking_Mosaic:
             mesh_size = int(size / 256 * 7 / 4)
             mesh_size = int(np.clip(mesh_size, 3, 13))
             mask = cv2.dilate(mask, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (mesh_size, mesh_size)))
-            bgr_copy = LibMasking_Mosaic.workOnSelected(bgr, bgr_copy, mask=mask)
+            bgr_copy = LibMasking_Mosaic.workOnSelected(canvas, bgr_copy, mask=mask)
             return bgr_copy, super_pixels
         else:
             lft, top, rig, bot = box
@@ -228,49 +228,49 @@ class LibMasking_Mosaic:
             # size = float(sum(part.shape[:2])) / 2  # (h+w)/2
             # n_div = int(size / 256 * n_div)
             mosaic_bgr, super_pixels = LibMasking_Mosaic.doWithMosaicPolygon(part, vis_boundary, n_div=n_div, super_pixels=super_pixels)
-            bgr_copy = np.copy(bgr)
+            bgr_copy = np.copy(canvas)
             bgr_copy[top:bot, lft:rig] = mosaic_bgr
             return bgr_copy, super_pixels
 
     @staticmethod
-    def inferenceWithBox(bgr, box, masking_option):
+    def inferenceWithBox(bgr, canvas, box, masking_option):
         parameters = masking_option.parameters
         mosaic_type = parameters['mosaic_type']
         focus_type = parameters['focus_type']
         super_pixels = parameters['super_pixels'] if 'super_pixels' in parameters else None
         if mosaic_type == 'mosaic_pixel_square':
             num_pixel = parameters['num_pixel'] if 'num_pixel' in parameters else 48
-            return LibMasking_Mosaic.inferenceBoxWithMosaicSquare(bgr, box, num_pixel, focus_type=focus_type)
+            return LibMasking_Mosaic.inferenceBoxWithMosaicSquare(bgr, canvas, box, num_pixel, focus_type=focus_type)
         if mosaic_type == 'mosaic_pixel_polygon':
             n_div = parameters['n_div'] if 'n_div' in parameters else 32
             vis_boundary = parameters['vis_boundary'] if 'vis_boundary' in parameters else False
             mosaic_bgr, super_pixels = LibMasking_Mosaic.inferenceBoxWithMosaicPolygon(
-                bgr, box, n_div, vis_boundary=vis_boundary, focus_type=focus_type, super_pixels=super_pixels)
+                bgr, canvas, box, n_div, vis_boundary=vis_boundary, focus_type=focus_type, super_pixels=super_pixels)
             parameters['super_pixels'] = super_pixels
             return mosaic_bgr
         # specific config
         if mosaic_type == 'mosaic_pixel_polygon_small':
             # n_div = parameters['n_div'] if 'n_div' in parameters else 8
             mosaic_bgr, super_pixels = LibMasking_Mosaic.inferenceBoxWithMosaicPolygon(
-                bgr, box, 24, vis_boundary=False, focus_type=focus_type, super_pixels=super_pixels)
+                bgr, canvas, box, 24, vis_boundary=False, focus_type=focus_type, super_pixels=super_pixels)
             parameters['super_pixels'] = super_pixels
             return mosaic_bgr
         if mosaic_type == 'mosaic_pixel_polygon_small_line':
             # n_div = parameters['n_div'] if 'n_div' in parameters else 8
             mosaic_bgr, super_pixels = LibMasking_Mosaic.inferenceBoxWithMosaicPolygon(
-                bgr, box, 16, vis_boundary=True, focus_type=focus_type, super_pixels=super_pixels)
+                bgr, canvas, box, 16, vis_boundary=True, focus_type=focus_type, super_pixels=super_pixels)
             parameters['super_pixels'] = super_pixels
             return mosaic_bgr
         if mosaic_type == 'mosaic_pixel_polygon_big':
             # n_div = parameters['n_div'] if 'n_div' in parameters else 16
             mosaic_bgr, super_pixels = LibMasking_Mosaic.inferenceBoxWithMosaicPolygon(
-                bgr, box, 24, vis_boundary=False, focus_type=focus_type, super_pixels=super_pixels)
+                bgr, canvas, box, 24, vis_boundary=False, focus_type=focus_type, super_pixels=super_pixels)
             parameters['super_pixels'] = super_pixels
             return mosaic_bgr
         if mosaic_type == 'mosaic_pixel_polygon_big_line':
             # n_div = parameters['n_div'] if 'n_div' in parameters else 16
             mosaic_bgr, super_pixels = LibMasking_Mosaic.inferenceBoxWithMosaicPolygon(
-                bgr, box, 24, vis_boundary=True, focus_type=focus_type, super_pixels=super_pixels)
+                bgr, canvas, box, 24, vis_boundary=True, focus_type=focus_type, super_pixels=super_pixels)
             parameters['super_pixels'] = super_pixels
             return mosaic_bgr
         raise NotImplementedError(parameters)

core/application/face_masking/libmasking_sticker.py
@@ -63,7 +63,6 @@ class LibMasking_Sticker:
         box_src = np.reshape(np.array(box, dtype=np.int32), (1, 4))
         box_cur = np.reshape(np.array(cache.box, dtype=np.int32), (-1, 4))
         iou = BoundingBox.computeIOU(boxes1=box_src, boxes2=box_cur)  # 1,N
-        print(iou.shape)
         return int(np.argmax(iou[0, :]))
 
     @staticmethod
@@ -83,7 +82,7 @@ class LibMasking_Sticker:
                 bar.update(1)
 
     @staticmethod
-    def inferenceWithBox(bgr, box, masking_option):
+    def inferenceWithBox(bgr, canvas, box, masking_option):
         sticker = masking_option.parameters
         if isinstance(sticker, np.ndarray):
             assert sticker.shape[2] == 3 or sticker.shape[2] == 4, sticker.shape[2]
@@ -108,13 +107,13 @@ class LibMasking_Sticker:
                 mask = sticker_mask.astype(np.float32) / 255.
                 fusion = part * (1 - mask) + sticker_bgr * mask
                 fusion_bgr = np.round(fusion).astype(np.uint8)
-                bgr_copy = np.copy(bgr)
+                bgr_copy = np.copy(canvas)
                 bgr_copy[top:bot, lft:rig, :] = fusion_bgr
                 return bgr_copy
         if isinstance(sticker, dict):
             sticker_image = sticker['bgr']
             if 'eyes_center' in sticker:
-                ratio = 1.
+                ratio = 0.2
                 h, w, c = bgr.shape
                 lft, top, rig, bot = box
                 hh = bot - top
@@ -135,11 +134,11 @@ class LibMasking_Sticker:
                 sticker_warped = cv2.warpAffine(sticker_image, matrix, **param)
                 sticker_warped_bgr, sticker_warped_alpha = sticker_warped[:, :, :3], sticker_warped[:, :, 3:4]
                 mask = sticker_warped_alpha.astype(np.float32) / 255.
-                fusion = bgr * (1 - mask) + sticker_warped_bgr * mask
+                fusion = canvas * (1 - mask) + sticker_warped_bgr * mask
                 fusion_bgr = np.round(fusion).astype(np.uint8)
                 return fusion_bgr
             if 'eyes_center_fix' in sticker:
-                ratio = 1.5
+                ratio = 0.2
                 h, w, c = bgr.shape
                 lft, top, rig, bot = box
                 hh = bot - top
@@ -164,7 +163,7 @@ class LibMasking_Sticker:
                 sticker_warped = cv2.warpAffine(sticker_image, transform.params[:2, :], **param)
                 sticker_warped_bgr, sticker_warped_alpha = sticker_warped[:, :, :3], sticker_warped[:, :, 3:4]
                 mask = sticker_warped_alpha.astype(np.float32) / 255.
-                fusion = bgr * (1 - mask) + sticker_warped_bgr * mask
+                fusion = canvas * (1 - mask) + sticker_warped_bgr * mask
                 fusion_bgr = np.round(fusion).astype(np.uint8)
                 return fusion_bgr
             if 'align' in sticker:
@@ -190,7 +189,7 @@ class LibMasking_Sticker:
                 sticker_warped = cv2.warpAffine(sticker_image, matrix, **param)
                 sticker_warped_bgr, sticker_warped_alpha = sticker_warped[:, :, :3], sticker_warped[:, :, 3:4]
                 mask = sticker_warped_alpha.astype(np.float32) / 255.
-                fusion = bgr * (1 - mask) + sticker_warped_bgr * mask
+                fusion = canvas * (1 - mask) + sticker_warped_bgr * mask
                 fusion_bgr = np.round(fusion).astype(np.uint8)
                 return fusion_bgr
             if 'box' in sticker:
@@ -211,7 +210,7 @@ class LibMasking_Sticker:
                 mask = sticker_mask.astype(np.float32) / 255.
                 fusion = part * (1 - mask) + sticker_bgr * mask
                 fusion_bgr = np.round(fusion).astype(np.uint8)
-                bgr_copy = np.copy(bgr)
+                bgr_copy = np.copy(canvas)
                 bgr_copy[top:bot, lft:rig, :] = fusion_bgr
                 return bgr_copy
             if 'paste' in sticker:
@@ -226,7 +225,7 @@ class LibMasking_Sticker:
                 result_alpha_resize = cv2.resize(result_alpha, (w_ori, h_ori))
                 result_alpha_resize = result_alpha_resize[:, :, np.newaxis] / 255.0
                 image_C_cartoon = result_nd_resize * result_alpha_resize + (1 - result_alpha_resize) * image_c
-                bgr_copy = np.copy(bgr)
+                bgr_copy = np.copy(canvas)
                 bgr_copy[ltrb[1]:ltrb[3], ltrb[0]:ltrb[2], :] = image_C_cartoon
                 return bgr_copy
 

core/application/face_masking/libmasking_worker.py
@@ -101,17 +101,18 @@ class MaskingVideoWorker(threading.Thread):
                 break  # bug for video
             # masking process
             counter += 1
+            canvas = np.copy(bgr)
             for _, (person, it) in enumerate(self.iterator_list):
                 if person.identity in self.options_dict:
                     info = it.next()
                     if info.index_frame == frame_index:
                         masking_option = self.options_dict[person.identity]
-                        bgr = self.trying(frame_index, bgr, info.box_face, masking_option)
+                        canvas = self.trying(frame_index, bgr, canvas, info.box_face, masking_option)
                         it.update()
                     if info.index_frame < frame_index:
                         it.update()
             self.worker_lock.update(self.num_seq, 1)
-            self.result_list.append((frame_index, bgr))
+            self.result_list.append((frame_index, canvas))
             frame_index += 1
             if self.verbose is True:
                 self.worker_lock.info('{:<4d}: {:<4d}({:<4d},{:<4d})'.format(self.num_seq, n, self.index_beg, self.index_end))