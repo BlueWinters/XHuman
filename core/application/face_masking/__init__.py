fe1f7e26a081a814c06046020bfca87cfdf5da23
xiaoye <hanxiongxiao@gmail.com>
2025-02-12 15:52:17 +0800

    update application.masking -- fix bug of crash

--- core/application/face_masking/__init__.py
+++ core/application/face_masking/__init__.py
@@ -1,7 +1,7 @@
 
 import logging
 
-__version__ = '0.2.13'
+__version__ = '0.2.14'
 logging.error('face_masking version: {}'.format(__version__))
 
 """
@@ -46,5 +46,7 @@ version - 0.2.12
     note: fix bug of rendering and task-2332
 version - 0.2.13
     note: fix task-2337
+version - 0.2.14
+    note: 2025-02-12, fix bug of crash
 """
 
