4b52e4b1e3bccd8cb696c6ce6b15cbc4ca23bdbd
xiaoye <hanxiongxiao@gmail.com>
2025-02-19 15:49:09 +0800

    update application.masking -- import错误

--- core/module.py
+++ core/module.py
@@ -25,7 +25,6 @@ GlobalModuleDictClass['function'] = LibFunction
 GlobalModuleDictClass['portrait_light'] = LibPortraitLight
 GlobalModuleDictClass['portrait_beauty'] = LibPortraitBeauty
 GlobalModuleDictClass['face_masking'] = LibMasking
-GlobalModuleDictClass['face_masking2'] = LibMasking2
 
 # third party
 GlobalModuleDictClass['insightface'] = LibInsightFace
