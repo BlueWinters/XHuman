update application.masking -- 图片打码bug修复

core/application/face_masking2/helper/masking_helper.py
@@ -4,6 +4,7 @@ import cv2
 import numpy as np
 import skimage
 from .cursor import AsynchronousCursor
+from .angle_helper import AngleHelper
 from ..scanning.scanning_image import InfoImage, InfoImage_Person
 from ..scanning.scanning_video import InfoVideo, InfoVideo_Person, InfoVideo_Frame
 from ....base import XPortrait, XPortraitHelper
@@ -57,42 +58,6 @@ class MaskingHelper:
         return max_mask
 
     @staticmethod
-    def getAngle(v):
-        if v[1] > 0:
-            r = abs(v[0]) / abs(v[1])
-            if v[0] < 0:
-                if r < 1:
-                    return 0
-                else:
-                    return 270
-            elif v[0] > 0:
-                if r < 1:
-                    return 0
-                else:
-                    return 90
-            else:
-                return 0
-        elif v[1] < 0:
-            r = abs(v[0]) / abs(v[1])
-            if v[0] < 0:
-                if r < 1:
-                    return 180
-                else:
-                    return 270
-            elif v[0] > 0:
-                if r < 1:
-                    return 180
-                else:
-                    return 90
-            else:
-                return 180
-        else:
-            if v[0] < 0:
-                return 270
-            else:
-                return 90
-
-    @staticmethod
     def getPortraitMaskingWithInfoImagePlus(bgr, info_image: InfoImage, option_dict, with_hair=True, expand=0.8):
         h, w, c = bgr.shape
         # segmentation
@@ -132,8 +97,7 @@ class MaskingHelper:
             # ajna = np.mean(info_person.landmark[17:27, :], axis=0)
             # angle = 0 if ajna[1] < info_person.landmark[30, 1] else 180  # or info_person.angle
             # angle = info_person.angle
-            vector = info_person.landmark[30, :] - np.mean(info_person.landmark[17:27, :], axis=0)
-            angle = MaskingHelper.getAngle(vector)
+            angle = AngleHelper.getAngleRollByLandmark(info_person.landmark)
             logging.info('identity-{}, angle-{}'.format(info_person.identity, angle))
             part = bgr_copy[top:bot, lft:rig]
             part_rot = GeoFunction.rotateImage(part, angle)

core/application/face_masking2/scanning/scanning_image.py
@@ -5,6 +5,7 @@ import os
 import cv2
 import numpy as np
 import json
+from ..helper.angle_helper import AngleHelper
 from .scanning_visor import ScanningVisor
 from ....base import XPortrait
 from ....geometry import Rectangle, GeoFunction
@@ -18,7 +19,7 @@ class InfoImage_Person:
         self.box = np.round(box).astype(np.int32)
         self.points = np.round(points).astype(np.int32)
         self.landmark = np.round(landmark).astype(np.int32)
-        self.angle = int(angle)
+        self.angle = AngleHelper.getAngleRollByLandmark(self.landmark)
 
     def formatAsDict(self) -> dict:
         return dict(
@@ -152,8 +153,8 @@ class InfoImage:
             bgr_c, box_c = self.autoRotateForCartoon(self.bgr, info_person.box, info_person.angle)
             preview_dict[info_person.identity] = dict(
                 # interface
-                image=self.bgr,
-                box=info_person.box.tolist(),
+                image=bgr_c,
+                box=box_c.tolist(),
                 face=self.cropPreviewFace(self.bgr, info_person, size, is_bgr, ext, auto_rot),
                 # for debug
                 angle=info_person.angle,